90128826b453e293c5a355f07a4e4cd76b204364
jaksamarko-sys <jaksa.marko@diak.szbi-pg.hu>
2024-11-18 10:47:45 +0100

    add az

--- 02_str_list_elteres.py
+++ 02_str_list_elteres.py
@@ -2,10 +2,10 @@
 
 # Elemeik (a karakterek) nem módosíthatóak!
 # Ez hibát okoz:
-sztring = 'Ismered ezt a számot?'
-sztring[0] = 'i'
+#sztring = 'Ismered ezt a számot?'
+#sztring[0] = 'i'
   
 lista =  ['I', 's', 'm', 'e', 'r', 'e', 'd']
 print(lista)
-lista{0} = 'i'
+lista[0] = 'i'
 print(lista)
--- 03_feladat_1.py
+++ 03_feladat_1.py
@@ -3,11 +3,13 @@
 
 megadott_mondat = input("Kérlek adj meg egy mondatot: ")
 
-if megadott_mondat[-1] == '.':
-    print('Ez a mondat kijelentő')
-elif megadott_mondat[-1] == '?':
+if megadott_mondat.endswith('...'):
+    print('Ez a mondat ...')
+elif megadott_mondat.endswith('?'):
     print('Ez a mondat kérdő')
-elif megadott_mondat[-1] == '!':
+elif megadott_mondat.endswith('!'):
     print('Ez a mondat felkiáltó/óhajtó/felszólító')
+elif megadott_mondat.endswith('.'):
+    print('Ez a mondat kijelentő')
 else:    
     print('Nem adtál meg mondatvégi írásjelet.')    
